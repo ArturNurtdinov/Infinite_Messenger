c8e21db232324455cf4506823dd80401272842a9
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-08-20 14:13:56 +0500

    Changed tag

--- app/src/main/java/com/topaz/easymessenger/models/LoginModel.kt
+++ app/src/main/java/com/topaz/easymessenger/models/LoginModel.kt
@@ -3,6 +3,7 @@ package com.topaz.easymessenger.models
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.topaz.easymessenger.contracts.LoginContract
+import com.topaz.easymessenger.views.LoginActivity
 
 class LoginModel(private val onLoginListener: LoginContract.OnLoginListener) : LoginContract.Model {
     override fun loginWithEmailAndPassword(email: String, password: String) {
@@ -11,7 +12,7 @@ class LoginModel(private val onLoginListener: LoginContract.OnLoginListener) : L
                 onLoginListener.onSuccess()
             }
             .addOnFailureListener {
-                Log.d("LOGIN", it.message)
+                Log.d(LoginActivity.TAG, it.message)
                 onLoginListener.onFailure(it.message.toString())
             }
     }
