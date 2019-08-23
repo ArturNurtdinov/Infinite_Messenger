a67af4cf0a8a72e0dfdcd2e0f582bb2ee68aab93
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-08-23 12:43:32 +0300

    Added fetching current user here

--- app/src/main/java/com/topaz/easymessenger/views/LatestMessagesActivity.kt
+++ app/src/main/java/com/topaz/easymessenger/views/LatestMessagesActivity.kt
@@ -6,15 +6,39 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.topaz.easymessenger.R
+import com.topaz.easymessenger.data.User
 
 class LatestMessagesActivity : AppCompatActivity() {
+
+    companion object {
+        var currentUser: User? = null
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_messages)
 
+        fetchCurrentUser()
         verifyUserIsLoggedIn()
+    }
+
+    private fun fetchCurrentUser() {
+        val uid = FirebaseAuth.getInstance().uid
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                currentUser = p0.getValue(User::class.java)
+            }
+
+        })
     }
 
     private fun verifyUserIsLoggedIn() {
