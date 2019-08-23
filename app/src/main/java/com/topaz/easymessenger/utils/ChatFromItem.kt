f7427eb827f64799ac9b5852acffbb5a7c2c44b5
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-08-23 12:21:23 +0300

    Added message text in rows

--- app/src/main/java/com/topaz/easymessenger/utils/ChatFromItem.kt
+++ app/src/main/java/com/topaz/easymessenger/utils/ChatFromItem.kt
@@ -3,13 +3,14 @@ package com.topaz.easymessenger.utils
 import com.topaz.easymessenger.R
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.chat_to_row.view.*
 
-class ChatFromItem: Item<ViewHolder>() {
+class ChatFromItem(private val message: String): Item<ViewHolder>() {
     override fun getLayout(): Int {
         return R.layout.chat_from_row
     }
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
-
+        viewHolder.itemView.message.text = message
     }
 }
--- app/src/main/java/com/topaz/easymessenger/utils/ChatToItem.kt
+++ app/src/main/java/com/topaz/easymessenger/utils/ChatToItem.kt
@@ -3,10 +3,11 @@ package com.topaz.easymessenger.utils
 import com.topaz.easymessenger.R
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.chat_to_row.view.*
 
-class ChatToItem: Item<ViewHolder>() {
+class ChatToItem(private val message: String): Item<ViewHolder>() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
-
+        viewHolder.itemView.message.text = message
     }
 
     override fun getLayout(): Int {
