50f985b80f7fd8ddee84c9a28db1182602fd963e
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-08-25 23:55:34 +0300

    Added constant class

---
+++ app/src/main/java/com/topaz/easymessenger/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.topaz.easymessenger.utils
+
+class Constants {
+    companion object {
+        const val DEFAULT_AVATAR_URL =
+            "https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-and-shapes-3/177800/129-512.png"
+    }
+}
