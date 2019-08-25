58ae0d1b288930a50e6a24a64526582397245687
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-08-26 00:23:06 +0300

    Added constants for notification channel

--- app/src/main/java/com/topaz/easymessenger/utils/Constants.kt
+++ app/src/main/java/com/topaz/easymessenger/utils/Constants.kt
@@ -4,5 +4,7 @@ class Constants {
     companion object {
         const val DEFAULT_AVATAR_URL =
             "https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-and-shapes-3/177800/129-512.png"
+        const val CHANNEL_ID = "ID01"
+        const val CHANNEL_NAME = "Chat"
     }
 }
