477d2553eee6945eff29c853b2511b3c41741e52
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-08-28 00:55:47 +0300

    Added search through users

--- app/src/main/java/com/topaz/easymessenger/views/NewMessageActivity.kt
+++ app/src/main/java/com/topaz/easymessenger/views/NewMessageActivity.kt
@@ -1,8 +1,13 @@
 package com.topaz.easymessenger.views
 
+import android.app.SearchManager
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.core.view.MenuItemCompat
 import com.topaz.easymessenger.R
 import com.topaz.easymessenger.adapters.UserItem
 import com.topaz.easymessenger.contracts.NewMessageContract
@@ -11,14 +16,18 @@ import com.topaz.easymessenger.presenters.NewMessagePresenter
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_message.*
+import java.util.*
 
 class NewMessageActivity : AppCompatActivity(), NewMessageContract.View {
     private val presenter = NewMessagePresenter(this)
     private val adapter = GroupAdapter<ViewHolder>()
+    private val userList = LinkedList<User>()
+
     companion object {
         const val TAG = "NEW_MESSAGE"
         const val USER_KEY = "USER_KEY"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
@@ -34,8 +43,34 @@ class NewMessageActivity : AppCompatActivity(), NewMessageContract.View {
         new_message_recycler.adapter = adapter
         presenter.fetchUsers()
     }
+
     override fun addUser(user: User) {
         adapter.add(UserItem(user))
-        adapter.notifyDataSetChanged()
+        userList.add(user)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+        val searchView =
+            MenuItemCompat.getActionView(menu?.findItem(R.id.search)) as android.widget.SearchView
+        searchView.setOnQueryTextListener(object : android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.clear()
+                userList.filter { it.username.contains(newText ?: return false, true) }.forEach {
+                    adapter.add(UserItem(it))
+                }
+                return true
+            }
+
+        })
+
+        val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val component = ComponentName(this, NewMessageActivity::class.java)
+        searchView.setSearchableInfo(manager.getSearchableInfo(component))
+        return super.onCreateOptionsMenu(menu)
     }
 }
