1cb0cb9aeed36ed71d90be6548ed393fa436b487
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-09-01 22:46:19 +0300

    Added MAX_USERNAME_LENGTH

--- app/src/main/java/com/infinitevoid/easymessenger/utils/Constants.kt
+++ app/src/main/java/com/infinitevoid/easymessenger/utils/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val DEFAULT_AVATAR_URL =
             "https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-and-shapes-3/177800/129-512.png"
         const val USER_KEY = "USER_KEY"
+        const val MAX_USERNAME_LENGTH = 30
     }
 }
