bd99cb5fb69b2997bc1f501a3bc187266fa0bbb6
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-09-15 22:08:45 +0300

    Changed to ChildEventListener

--- app/src/main/java/com/infinitevoid/easymessenger/models/NewMessageModel.kt
+++ app/src/main/java/com/infinitevoid/easymessenger/models/NewMessageModel.kt
@@ -8,31 +8,37 @@ import com.infinitevoid.easymessenger.data.User
 import com.infinitevoid.easymessenger.views.NewMessageActivity
 
 class NewMessageModel(private val changeListener: NewMessageContract.ChangeListener) :
-    NewMessageContract.Model, ValueEventListener {
-    private var ref : DatabaseReference? = null
+    NewMessageContract.Model, ChildEventListener {
+
+    private var ref: DatabaseReference? = null
     override fun fetchUsers() {
         ref = FirebaseDatabase.getInstance().getReference("/users")
         ref?.keepSynced(false)
-        ref?.addValueEventListener(this)
+        ref?.addChildEventListener(this)
     }
 
     override fun onDestroy() {
         ref?.removeEventListener(this)
     }
 
-    override fun onCancelled(p0: DatabaseError) {
-
-    }
-
-    override fun onDataChange(p0: DataSnapshot) {
+    override fun onChildAdded(p0: DataSnapshot, p1: String?) {
         val matchingUid = FirebaseAuth.getInstance().uid
-        p0.children.forEach {
-            val user = it.getValue(User::class.java)
-            Log.d(NewMessageActivity.TAG, "User with uid: ${user?.uid} fetched")
-            if ((user != null) && (user.uid != matchingUid)) {
-                changeListener.getChange(user)
-            }
+        val user = p0.getValue(User::class.java)
+        Log.d(NewMessageActivity.TAG, "User with uid: ${user?.uid} fetched")
+        if ((user != null) && (user.uid != matchingUid)) {
+            changeListener.getChange(user)
         }
     }
 
+    override fun onCancelled(p0: DatabaseError) {
+    }
+
+    override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+    }
+
+    override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+    }
+
+    override fun onChildRemoved(p0: DataSnapshot) {
+    }
 }
