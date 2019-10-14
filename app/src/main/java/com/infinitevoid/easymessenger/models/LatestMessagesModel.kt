1dbc48ee7ec0eeee3423ce992ec3ca214733189c
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-10-14 23:44:07 +0300

    Removed notifications

--- app/src/main/java/com/infinitevoid/easymessenger/models/LatestMessagesModel.kt
+++ app/src/main/java/com/infinitevoid/easymessenger/models/LatestMessagesModel.kt
@@ -35,29 +35,12 @@ class LatestMessagesModel(private val onDataReady: LatestMessagesContract.OnData
         val refUserMesFromTo =
             firebaseInstance
                 .getReference("/user-messages/${message.fromId}/${message.toId}/${message.id}/read")
-        val copied = ChatMessage(
-            message.id,
-            message.message,
-            message.fromId,
-            message.toId,
-            message.timestamp,
-            message.imageURL,
-            message.read
-        )
         message.read = "true"
 
         refLatestFromTo.setValue("true")
         refLatestToFrom.setValue("true")
         refUserMesFromTo.setValue("true")
         refUserMesToFrom.setValue("true")
-
-        if (messagesMap.containsValue(copied)) {
-            messagesMap.forEach {
-                if (it.value == copied) {
-                    it.value.read = "true"
-                }
-            }
-        }
     }
 
     override fun verifyIsLogged(): Boolean {
@@ -68,7 +51,6 @@ class LatestMessagesModel(private val onDataReady: LatestMessagesContract.OnData
         FirebaseAuth.getInstance().signOut()
     }
 
-    private var messagesMap = HashMap<String, ChatMessage>()
     override fun fetchCurrentUser() {
         val uid = FirebaseAuth.getInstance().uid
         val ref = firebaseInstance.getReference("/users/$uid")
@@ -89,47 +71,6 @@ class LatestMessagesModel(private val onDataReady: LatestMessagesContract.OnData
         ref?.addChildEventListener(this)
     }
 
-    override fun onDestroy() {
-    }
-
-    override fun saveMap(context: Context) {
-        val prf = context.getSharedPreferences("USERDATA", Context.MODE_PRIVATE)
-        if (prf != null) {
-            val keys = mutableListOf<String>()
-            messagesMap.forEach {
-                if (it.value.read == "true") {
-                    keys.add(it.key)
-                }
-            }
-            keys.forEach {
-                messagesMap.remove(it)
-            }
-            Log.d("Notification!!!", "Before saving size: ${messagesMap.size}")
-            if (messagesMap.isNotEmpty()) {
-                val editor = prf.edit()
-                val gson = Gson()
-                val json = gson.toJson(messagesMap)
-                editor.remove("MAP").apply()
-                editor.putString("MAP", json).commit()
-            }
-        }
-    }
-
-    override fun loadMap(context: Context) {
-        val prf = context.getSharedPreferences("USERDATA", Context.MODE_PRIVATE)
-        try {
-            if (prf != null) {
-                val gson = Gson()
-                val json = prf.getString("MAP", "")
-                messagesMap =
-                    gson.fromJson(json, object : TypeToken<HashMap<String, ChatMessage>>() {}.type)
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-    }
-
     override fun onCancelled(p0: DatabaseError) {
     }
 
@@ -142,43 +83,10 @@ class LatestMessagesModel(private val onDataReady: LatestMessagesContract.OnData
     override fun onChildChanged(p0: DataSnapshot, p1: String?) {
         val message = p0.getValue(ChatMessage::class.java) ?: return
         onDataReady.onLatestChanged(message, p0.key!!)
-        if ((message.read == "false") && (message.fromId != FirebaseAuth.getInstance().uid) && (messagesMap[p0.key!!]?.message != message.message)) {
-            setListenerForNotifications(message, p0.key!!)
-        }
     }
 
     override fun onChildAdded(p0: DataSnapshot, p1: String?) {
         val message = p0.getValue(ChatMessage::class.java) ?: return
         onDataReady.onLatestAdded(message, p0.key!!)
-        if ((message.read == "false") && (message.fromId != FirebaseAuth.getInstance().uid) && (messagesMap[p0.key!!]?.message != message.message)) {
-            setListenerForNotifications(message, p0.key!!)
-        }
-    }
-
-    private fun setListenerForNotifications(message: ChatMessage, key: String) {
-        val ref = FirebaseDatabase.getInstance()
-            .getReference("/latest-messages/${message.toId}/${message.fromId}/text")
-        ref.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
-            }
-
-            override fun onDataChange(p0: DataSnapshot) {
-                fetchUserForNotification(message)
-            }
-        })
-        messagesMap[key] = message
-    }
-
-    private fun fetchUserForNotification(message: ChatMessage) {
-        val ref = FirebaseDatabase.getInstance().getReference("/users/${message.fromId}")
-        ref.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
-            }
-
-            override fun onDataChange(p0: DataSnapshot) {
-                onDataReady.setNotification(message, p0.getValue(User::class.java))
-            }
-
-        })
     }
 }
